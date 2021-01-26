d1b049083f39986292b3f4696342af2c99f0787c
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-01-26 17:36:00 -0300

    primeira aula

---
+++ Introdução.sql
@@ -0,0 +1,15 @@
+//Criando um banco de dados
+
+CREATE DATABASE cadastro;
+-> Cria o banco de dados
+
+CREATE TABLE pessoas (
+    nome varchar(30),
+    idade tinyint,
+    sexo char(1),
+    peso float,
+    altura float,
+    nacionalidade varchar(20)
+);
+-> Criando uma Table com essas caracteristicas
+
