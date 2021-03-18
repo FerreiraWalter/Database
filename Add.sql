01dce4a2d1b574d39f92db7f46a4927f79ae0fb7
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2021-03-18 13:49:15 -0300

    add itens na Tabela

---
+++ Add.sql
@@ -0,0 +1,7 @@
+-> Utilizando Php->PDO 
+
+$sql = $pdo->prepare('INSERT INTO pessoa(nome, telefone, email) VALUES(?, ?, ?)');
+    $sql->execute(array("Valter", "89996928345", "wferreirarjr@gmail.com"));
+
+    #2ª Forma:
+    //$pdo->query('INSERT INTO pessoa(nome, telefone, email) VALUES("Walter", "79996928345", "wferreirarjr@gmail.com")');
