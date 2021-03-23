35f73182af38a1c8d30cda0342469cfd89b0d45a
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2021-03-23 12:43:24 -0300

    Update Add.sql

--- Add.sql
+++ Add.sql
@@ -1,7 +1,8 @@
 -> Utilizando Php->PDO 
 
+#1ª Forma: (Mais segura)
 $sql = $pdo->prepare('INSERT INTO pessoa(nome, telefone, email) VALUES(?, ?, ?)');
     $sql->execute(array("Valter", "89996928345", "wferreirarjr@gmail.com"));
 
-    #2ª Forma:
-    //$pdo->query('INSERT INTO pessoa(nome, telefone, email) VALUES("Walter", "79996928345", "wferreirarjr@gmail.com")');
+#2ª Forma:
+$pdo->query('INSERT INTO pessoa(nome, telefone, email) VALUES("Walter", "79996928345", "wferreirarjr@gmail.com")');
